ab407a4f2fca8baa48f41b2cedbf43d3b0bab918
Chunga changa <mondeblum@gmail.com>
2021-12-04 20:14:53 +0300

    sq

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = randint(0, 300)
+        w = randint(0, 300 - x)
+        y = randint(0, 210)
+        h = randint(0, 210 - y)
+        qp.drawEllipse(x, y, w, h)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
